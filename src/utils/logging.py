f20dab8615cfe815b13f095b24c352c074f45bee
Andrea Nuzzo <andrea@MBP-di-Andrea.fritz.box>
2024-04-25 15:54:29 +0200

    fix: error ahndler logging

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -5,16 +5,18 @@ def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)  # Imposta il livello minimo di log a DEBUG
 
-    # Formato per i messaggi di log
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # Se non ci sono handler, configura il logging
+    if not logger.handlers:
+        # Formato per i messaggi di log
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # Crea un StreamHandler per stampare i log nel terminale
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    stream_handler.setLevel(logging.DEBUG)  # Imposta il livello per il terminale
+        # Crea un StreamHandler per stampare i log nel terminale
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        stream_handler.setLevel(logging.DEBUG)  # Imposta il livello per il terminale
 
-    # Aggiungi il StreamHandler al logger
-    logger.addHandler(stream_handler)
+        # Aggiungi il StreamHandler al logger
+        logger.addHandler(stream_handler)
 
     # # Crea un FileHandler per scrivere i log in un file
     # file_handler = logging.FileHandler('app.log')
