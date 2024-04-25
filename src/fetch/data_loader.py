5f9a7a8174b0ce91f92c186220ff047b81130343
Andrea Nuzzo <andrea@MBP-di-Andrea.fritz.box>
2024-04-25 12:52:22 +0200

    feat: add logging

--- src/fetch/data_loader.py
+++ src/fetch/data_loader.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import logging
+from utils.logging import setup_logging
+
+setup_logging()
 
 class DataLoader:
     def __init__(self, file_path):
@@ -7,7 +11,7 @@ class DataLoader:
     def load_data(self):
         if self.file_path.endswith('.xlsx'): 
             data = pd.read_excel(self.file_path)
-            print(data)
+            logging.debug(f"Uploaded data from the file {self.file_path}")
             return data
         raise ValueError("The file format must be Excel with the extension '.xlsx'")
             
